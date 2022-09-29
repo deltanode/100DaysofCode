9e2a1c7f327153e473d6dfa9f1e7e5ae3066eb7c
Yogesh <yogesh7132@gmail.com>
2022-09-29 16:46:58 +0530

    Added state for the form

--- 08.3-react-getting-started/src/Main.js
+++ 08.3-react-getting-started/src/Main.js
@@ -1,17 +1,9 @@
 import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 
-const pets = [
-  { name: "meowsalot", species: "cat", age: 5, id: "1" },
-  { name: "barkalot", species: "dog", age: 6, id: "2" },
-  { name: "fluffy", species: "rabbit", age: 4, id: "3" },
-  { name: "pursslot", species: "cat", age: 3, id: "4" },
-  { name: "queesalot", species: "cat", age: 2, id: "5" }
-]
-
 // Header Component
 function Header() {
-  return <h1>Application</h1>
+  return <h1 className="orange">Application</h1>
 }
 
 // Component to display current time (using useState)
@@ -54,7 +46,6 @@ function Like() {
   }
   return (
     <>
-      <hr />
       <button onClick={increaseLike}>Increase Like</button>
       <button onClick={decreaseLike}>Decrease Like</button>
       <h2>The page has {likeCount} likes</h2>
@@ -64,16 +55,25 @@ function Like() {
 
 // Component to add pets in list using form
 function AddPetForm(props) {
+  const [name, setName] = useState()
+  const [species, setSpecies] = useState()
+  const [age, setAge] = useState()
+
   function handleSubmit(e) {
     e.preventDefault()
+    props.setPets(prev => prev.concat({ name, species, age, id: Date.now() }))
+    setName("")
+    setSpecies("")
+    setAge("")
   }
+
   return (
     <form onSubmit={handleSubmit}>
       <fieldset>
         <legend>Add New Pet</legend>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Species" />
-        <input type="text" placeholder="Age in years" />
+        <input value={name} onChange={e => setName(e.target.value)} placeholder="Name" />
+        <input value={species} onChange={e => setSpecies(e.target.value)} placeholder="Species" />
+        <input value={age} onChange={e => setAge(e.target.value)} placeholder="Age in years" />
         <button>Add Pet</button>
       </fieldset>
     </form>
@@ -82,11 +82,19 @@ function AddPetForm(props) {
 
 // Main Component
 function Main() {
+  const [pets, setPets] = useState([
+    { name: "meowsalot", species: "cat", age: 5, id: "1" },
+    { name: "barkalot", species: "dog", age: 6, id: "2" },
+    { name: "fluffy", species: "rabbit", age: 4, id: "3" },
+    { name: "pursslot", species: "cat", age: 3, id: "4" },
+    { name: "queesalot", species: "cat", age: 2, id: "5" }
+  ])
   return (
     <>
       <Header />
       <Time />
-      <AddPetForm />
+      <Like />
+      <AddPetForm setPets={setPets} />
       {/* {["Hello", "Hi"]} */}
       {/* {[<li>Hello</li>, <li>Hi</li>]} */}
       <ul>
@@ -94,8 +102,6 @@ function Main() {
           <Pet name={pet.name} species={pet.species} age={pet.age} key={pet.id} />
         ))}
       </ul>
-      <Like />
-      z``
     </>
   )
 }
